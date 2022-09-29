c2ce694ed5211dbe00862a200c30079442872257
traderT <69872754+TheSeriousTrader@users.noreply.github.com>
2022-09-29 10:42:03 +0200

    Update analysis.py

--- zmqSimulation/analysis.py
+++ zmqSimulation/analysis.py
@@ -5,31 +5,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # data
-
 data = pd.read_csv('log.csv', index_col = 0, parse_dates = True)
-
 data.index = pd.to_datetime(data.index * 1000, unit = 'ms')
 
 
-# ms
-
+# times
 data['td'] = data.index.to_series().diff()
-
-ms = []
-
+times = []
 for i in range(len(data)):
-
     if data['log'].iloc[i] == 'dataReceived':
-        
-        if data['td'][i].microseconds < 200:
-            
-            ms.append(data['td'][i].microseconds)
- 
-ms.sort()
+        times.append(data['td'][i].microseconds)
+times.sort()
 
-print(' ')
-print('average:', np.mean(ms))
-print('std:', np.std(ms))
+# analysis
+print('average:', np.mean(times))
+print('std:', np.std(times))
 
-plt.hist(ms, bins = 100)
+# plot
+plt.hist(times, bins = 100)
 plt.show()
